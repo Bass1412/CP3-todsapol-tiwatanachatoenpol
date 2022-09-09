1bf4312eefe318f84967e9e83ac3e6886bcbec11
todsapol <bass.todsapol@gmail.com>
2022-09-09 15:54:31 +0700

    HW Score

---
+++ Exercise4_todsapol_t.py
@@ -0,0 +1,10 @@
+print("---- your score ----")
+fe = 85
+gb = 75
+itc = 76.5
+cp  = 95
+
+print("Foundation English" ,":", 85)
+print("Introduction to Computer Systems" ,":", 75)
+print("Introduction to Computer Systems",":",76.5)
+print("Computer Programming" ,":", 95)
