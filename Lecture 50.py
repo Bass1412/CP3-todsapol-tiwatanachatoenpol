cff23c9e3c442003d02f9ff55dbc12ae9820213d
todsapol <bass.todsapol@gmail.com>
2022-09-14 21:49:51 +0700

    ADD miniHW

---
+++ Lecture 50.py
@@ -0,0 +1,13 @@
+def Addnumber (a,b):
+
+    print(a+b)
+def subtractionnumber (a,b):
+    print(a-b)
+def multiply (a,b):
+    print(a*b)
+def divide (a,b):
+    print(a/b)
+Addnumber(5,5)
+subtractionnumber(5,5)
+multiply(10,100)
+divide(100,3)
